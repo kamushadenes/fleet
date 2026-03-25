1e2cd739d9ff7e8644e1d9a7f4d5a351388dc403
Scott Gress <scott@fleetdm.com>
2026-03-25 10:33:29 -0500

    refactor

--- cmd/fleetctl/fleetctl/generate_gitops_test.go
+++ cmd/fleetctl/fleetctl/generate_gitops_test.go
@@ -1092,7 +1092,7 @@ func TestGenerateSoftwareAutoUpdateSchedule(t *testing.T) {
 	cmd.FilesToWrite["fleets/test.yml"] = map[string]any{}
 
 	// generate software for team 1
-	res, err := cmd.generateSoftware("fleets/test.yml", 1, "team-a", false, false)
+	res, err := cmd.generateSoftware("fleets/test.yml", 1, "team-a", false)
 	require.NoError(t, err)
 	require.NotNil(t, res)
 
@@ -1479,7 +1479,7 @@ func TestGenerateSoftware(t *testing.T) {
 		SoftwareList: make(map[uint]Software),
 	}
 
-	softwareRaw, err := cmd.generateSoftware("team.yml", 1, "some-team", false, false)
+	softwareRaw, err := cmd.generateSoftware("team.yml", 1, "some-team", false)
 	require.NoError(t, err)
 	require.NotNil(t, softwareRaw)
 	var software map[string]any
@@ -1558,7 +1558,7 @@ func TestGenerateSoftwareScriptPackages(t *testing.T) {
 		SoftwareList: make(map[uint]Software),
 	}
 
-	softwareRaw, err := cmd.generateSoftware("team.yml", 2, "script-team", false, false)
+	softwareRaw, err := cmd.generateSoftware("team.yml", 2, "script-team", false)
 	require.NoError(t, err)
 	require.NotNil(t, softwareRaw)
 
